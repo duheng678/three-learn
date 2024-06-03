7074e0ca2132bb475cb2e19eb1b65982f709788e
duheng <duheng678@163.com>
2024-06-03 17:04:07 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+//创建场景
+const scene = new THREE.Scene()
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+//创建相机
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+//轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+//设置带阻尼的惯性
+controls.enableDamping = true
+controls.autoRotate = true
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set(0, 20, 100)
+//创建渲染器
+
+//创建几何体
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+//创建材质
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+//创建网格
+const cube = new THREE.Mesh(geometry, material)
+
+scene.add(cube)
+
+camera.position.z = 5
+camera.position.y = 2
+camera.position.x = 2
+camera.lookAt(1, 0, 0)
+
+//添加世界坐标辅助线
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+function animate() {
+  console.log('???')
+
+  requestAnimationFrame(animate)
+  controls.update()
+
+  renderer.render(scene, camera)
+}
+animate()
